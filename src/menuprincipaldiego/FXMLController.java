6b54f305a88ce0149dd26df39c8ad8280f54938c
jojutom <159322976+jojutom@users.noreply.github.com>
2026-05-20 20:22:21 +0200

    Fix

---
+++ src/menuprincipaldiego/FXMLController.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package menuprincipaldiego;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author jrags
+ */
+public class FXMLController implements Initializable {
+
+    @FXML
+    private BorderPane contenedorPrincipal;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void mostrarPantallaPerfil(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleMouseEntered(MouseDragEvent event) {
+    }
+
+    @FXML
+    private void handleMouseExited(MouseDragEvent event) {
+    }
+    
+}
